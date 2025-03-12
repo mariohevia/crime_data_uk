c43d5948316bbcd7ba7ba7d864ee7d6b516076b8
Mario Hevia <mario_hevia@hotmail.com>
2025-03-12 18:38:51 +0000

    Improved map tooltips and pills

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from utils.crime_data_fetch import CATEGORIES
+from utils.crime_data_fetch import FROM_PRETTY_CATEGORIES
 import streamlit as st
 
 def add_pills_filter_df(df=pd.DataFrame()):
-    selection = st.pills("Crime Category", CATEGORIES, selection_mode="multi", default=CATEGORIES)
+    pretty_selection = st.pills("Crime Category", FROM_PRETTY_CATEGORIES.keys(), selection_mode="multi", default=FROM_PRETTY_CATEGORIES.keys())
+    selection = [FROM_PRETTY_CATEGORIES[cat] for cat in pretty_selection]
     if df.shape[0] != 0:
         filtered_df = df[df['category'].isin(selection)].copy()
         return filtered_df
